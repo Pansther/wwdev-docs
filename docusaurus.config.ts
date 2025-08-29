68a95a680d25547e4c1d4601de43c4907d17ea60
Pansther <werawit_boat@windowslive.com>
2025-08-29 09:23:46 +0700

    build: Update base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs.wwdev.vercel.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "https://github.com/Pansther", // Usually your GitHub org/user name.
-  projectName: "https://github.com/Pansther/wwdev-docs", // Usually your repo name.
+  organizationName: "Pansther", // Usually your GitHub org/user name.
+  projectName: "wwdev-docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
